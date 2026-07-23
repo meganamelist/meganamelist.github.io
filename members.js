637a36ffc30e7bf43d8bbf55fb35951e8b1cd311
meganamelist <meganamelist@gmail.com>
2026-07-23 22:15:17 +0700

    Update members.js

--- members.js
+++ members.js
@@ -8,10 +8,10 @@ const membersData = [
     id: 2,
     name: "Test",
     facebook: "https://facebook.com/your.facebook.2"
-  }
+  },
   {
     id: 3,
     name: "Test",
     facebook: "https://facebook.com/your.facebook.3"
-  }
+  },
 ];
