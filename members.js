31f43bd45612b2e5e203cf3bbabbbb8465577bb8
meganamelist <meganamelist@gmail.com>
2026-07-23 23:17:11 +0700

    Add files via upload

--- members.js
+++ members.js
@@ -2,16 +2,31 @@ const membersData = [
   {
     id: 1,
     name: "Jack Megastrongest",
-    facebook: "https://www.facebook.com/jack.beyondgod.1"
+    facebook: "https://www.facebook.com/jack.beyondgod .1"
   },
   {
     id: 2,
     name: "Mino Megastrongest",
-    facebook: "https://www.facebook.com/minosmp.2"
+    facebook: "https://www.facebook.com/minosmp .2"
   },
   {
     id: 3,
     name: "Job Megastrongest",
-    facebook: "https://www.facebook.com/jobavartanbxy.3"
+    facebook: "https://www.facebook.com/jobavartanbxy .3"
+  }
+{
+    id: 4,
+    name: "Perce Megastrongest",
+    facebook: "https://www.facebook.com/profile.php?id=61583367501844 .4"
+  }
+{
+    id: 5,
+    name: "Henry Megastrongest",
+    facebook: "https://www.facebook.com/henry.Donetobyungyim .5"
+  }
+{
+    id: 6,
+    name: "Yoksod Megastrongest",
+    facebook: "https://www.facebook.com/yoksod.highbizzard.3"
   }
 ];
