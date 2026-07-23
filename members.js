2088b85a346065e7ee94c32b4518d4979f159c68
meganamelist <meganamelist@gmail.com>
2026-07-23 23:35:04 +0700

    Add files via upload

--- members.js
+++ members.js
@@ -1,37 +1,17 @@
 const membersData = [
   {
     id: 1,
-    name: "Jack Megastrongest",
-    facebook: "https://www.facebook.com/jack.beyondgod .1"
+    name: "Test",
+    facebook: "https://facebook.com/your.facebook.1"
   },
   {
     id: 2,
-    name: "Mino Megastrongest",
-    facebook: "https://www.facebook.com/minosmp .2"
-  },
+    name: "Test",
+    facebook: "https://facebook.com/your.facebook.2"
+  }
   {
     id: 3,
-    name: "Job Megastrongest",
-    facebook: "https://www.facebook.com/jobavartanbxy .3"
-  }
-{
-    id: 4,
-    name: "Perce Megastrongest",
-    facebook: "https://www.facebook.com/profile.php?id=61583367501844 .4"
-  }
-{
-    id: 5,
-    name: "Henry Megastrongest",
-    facebook: "https://www.facebook.com/henry.Donetobyungyim .5"
-  }
-{
-    id: 6,
-    name: "Yoksod Megastrongest",
-    facebook: "https://www.facebook.com/yoksod.highbizzard.3"
-  }
-{
-    id: 7,
-    name: "Nxngmixg Megastrongest",
-    facebook: "https://www.facebook.com/mungming.wtflevaronz.3"
+    name: "Test",
+    facebook: "https://facebook.com/your.facebook.3"
   }
 ];
