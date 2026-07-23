35a6a226dc2abfcf010cd33bfcdede732715be04
meganamelist <meganamelist@gmail.com>
2026-07-23 23:45:08 +0700

    Update members.js

--- members.js
+++ members.js
@@ -2,16 +2,16 @@ const membersData = [
   {
     id: 1,
     name: "JACK MEGASTRONGEST",
-    facebook: "https://www.facebook.com/jack.beyondgod/.1"
+    facebook: "https://www.facebook.com/jack.beyondgod/.1",
   },
   {
     id: 2,
     name: "MINO MEGASTRONGEST",
-    facebook: "https://www.facebook.com/minosmp.2"
+    facebook: "https://www.facebook.com/minosmp.2",
   }
   {
     id: 3,
     name: "JOB MEGASTRONGEST",
-    facebook: "https://www.facebook.com/jobavartanbxy.3"
+    facebook: "https://www.facebook.com/jobavartanbxy.3",
   }
 ];
